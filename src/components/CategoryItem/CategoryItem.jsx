b3660efb01e0b14467088f398ba1aa25ffcb3f59
synerf <ashishpimpre.connect@gmail.com>
2023-07-01 15:39:38 +0530

    initial

---
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -0,0 +1,20 @@
+import "./CategoryItem.scss";
+
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Show Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
---
+++ src/routes/Home/Home.jsx
@@ -0,0 +1,39 @@
+import Directory from "../../components/Directory/Directory";
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "HATS",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "JACKETS",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "SNEAKERS",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "WOMEN",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "MEN",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+
+  return (
+    <div>
+      <Directory categories={categories} />
+    </div>
+  );
+};
+
+export default Home;
